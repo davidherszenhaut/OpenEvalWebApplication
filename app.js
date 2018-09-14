637b7332b9ceb6686cd3ac4567f19e05c50e5d8f
David Herszenhaut <david.herszenhaut@gmail.com>
2018-09-14 05:05:28 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+// express set up
+
+var express = require('express');
+var app = express();
+var router = express.Router();
+
+var path = __dirname + '/views/';
+
+// routing for views
+
+app.use('/', router);
+
+router.get('/', function(req, res) {
+    res.sendFile(path + 'index.html');
+});
+
+router.get('/all-classes', function(req, res) {
+    res.sendFile(path + 'all-classes.html');
+});
+
+router.get('/about', function(req, res) {
+    res.sendFile(path + 'about.html');
+});
+
+app.use('*', function(req, res) {
+    res.send('404 Not Found');
+});
+
+// logic from ./js/ files
+
+var allClasses = require('./js/all-classes.js');
+allClasses.getClasses('https://openeval-server.herokuapp.com/classes');
+
+// start local server
+
+app.listen(3000, function() {
+    console.log('Live on port 3000');
+});
---
+++ js/all-classes.js
@@ -0,0 +1,40 @@
+var https = require('https');
+var fs = require('fs');
+var jsdom = require('jsdom');
+
+module.exports = {
+    inputListener: function(searchString) {
+        console.log('testing');
+        app.post('/courseSearch', function(req, res) {
+            console.log(req.body.course);
+        });
+    },
+    getClasses: function(url) {
+        https.get(url, function(res) {
+            var body = '';
+            res.on('data', function(chunk) {
+                body += chunk;
+            });
+            res.on('end', function() {
+                var res = JSON.parse(body);
+                classList = '';
+                courses = res.message;
+                for (var i = 0; i < courses.length - 1; i++) {
+                    classList += '<li>' + courses[i].courseNumber + ':' + courses[i].courseName + '</li>\n';
+                }
+                fs.appendFile('./views/all-classes.html', classList, function(err) {
+                    if (err) throw err;
+                });
+            });
+        }).on('error', function(err) {
+            console.log(err);
+        });
+    }
+};
+
+/*
+
+go back to form style
+make new page same look as all-classes but only display matching search term
+
+*/
