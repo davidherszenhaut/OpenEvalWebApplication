ea7f382c96d1828648db4c50439e81547891bf05
David Herszenhaut <david.herszenhaut@gmail.com>
2018-11-12 01:18:13 -0500

    Began working on comments and documentation

--- public/js/all-classes.js
+++ public/js/all-classes.js
@@ -1,84 +1,105 @@
-var https = require('https');
-// const jsdom = require('jsdom');
-// const { JSDOM } = jsdom;
+const https = require('https');
+const handlebars = require('handlebars');
+const fs = require('fs');
 
-// const { document } = (new JSDOM('<!doctype html><html><body><div id="class-list"></div></body></html>')).window;
-
-function getClasses(url) {
+/**
+ *
+ * Gets all available courses seen at the given url and
+ * constructs a Handlebars template according to the data.
+ *
+ * @param  url  string, API endpoint pointing to all available courses
+ * @return none
+ *
+ */
+function getAllCourses(url) {
+    // get request to API endpoint
     https.get(url, function(res) {
         var body = '';
         res.on('data', function(chunk) {
             body += chunk;
         });
+        // when data from endpoint has been retrieved
         res.on('end', function() {
-            var res = JSON.parse(body);
-            classList = '';
-            courses = res.message;
-            for (var i = 0; i < courses.length - 1; i++) {
-                console.log(courses[i].courseNumber + ": " + courses[i].courseName);
-                // var node = document.createElement("p");
-                // var text = document.createTextNode(courses[i].courseNumber + ": " + courses[i].courseName);
-                // node.appendChild(text);
-                // document.getElementById("class-list").appendChild(node);
-            }
+            const res = JSON.parse(body);
+            // set input and output files for Handlebars template
+            const inFile = 'views/all-classes.hbs';
+            const outFile = 'views/all-classes.html';
+            const data = res.message;
+            const source = fs.readFileSync(inFile, 'utf8');
+            const template = handlebars.compile(source, {strict: true});
+            const result = template(data);
+            // console.log(result);
+            fs.writeFileSync(outFile, result);
         });
     }).on('error', function(err) {
         console.log(err);
     });
 }
 
-getClasses('https://openeval-server.herokuapp.com/classes');
-
 module.exports = {
-    // listens to user input for live search functionality
-    inputListener: function(searchString) {
-        console.log('testing');
-        app.post('/courseSearch', function(req, res) {
-            console.log(req.body.course);
-        });
-    },
-    // old function for getting classes, writes text to file, not proper HTML
-    getClasses: function(url) {
+    /**
+     *
+     * Gets all available courses seen at the given url and
+     * constructs a Handlebars template according to the data.
+     *
+     * @param  url  string, API endpoint pointing to all available courses
+     * @return none
+     *
+     */
+    getAllCourses: function(url) {
+        // get request to API endpoint
         https.get(url, function(res) {
             var body = '';
             res.on('data', function(chunk) {
                 body += chunk;
             });
+            // when data from endpoint has been retrieved
             res.on('end', function() {
-                var res = JSON.parse(body);
-                classList = '';
-                courses = res.message;
-                for (var i = 0; i < courses.length - 1; i++) {
-                    classList += '<li>' + courses[i].courseNumber + ':' + courses[i].courseName + '</li>\n';
-                }
-                fs.appendFile('./views/all-classes.html', classList, function(err) {
-                    if (err) throw err;
-                });
-            });
-        }).on('error', function(err) {
-            console.log(err);
-        });
-    },
-    // new function for getting classes, look into cheerio and jsdom
-    getClassesJSDOM: function(url) {
-        https.get(url, function(res) {
-            var body = '';
-            res.on('data', function(chunk) {
-                body += chunk;
-            });
-            res.on('end', function() {
-                var res = JSON.parse(body);
-                classList = '';
-                courses = res.message;
-                for (var i = 0; i < courses.length - 1; i++) {
-                    console.log("Course Number: " + courses[i].courseNumber + ", Course Name: " + courses[i].courseName);
-                    const $ = cheerio.load("<p>Course Number: " + courses[i].courseNumber + ", Course Name: " + courses[i].courseName + "</p>");
-                    $("<p>Course Number: " + courses[i].courseNumber + ", Course Name: " + courses[i].courseName + "</p>").appendTo('#class-list');
-                    $.html();
-                }
+                const res = JSON.parse(body);
+                // set input and output files for Handlebars template
+                const inFile = 'views/all-classes.hbs';
+                const outFile = 'views/all-classes.html';
+                const data = res.message;
+                const source = fs.readFileSync(inFile, 'utf8');
+                const template = handlebars.compile(source, {strict: true});
+                const result = template(data);
+                // console.log(result);
+                fs.writeFileSync(outFile, result);
             });
         }).on('error', function(err) {
             console.log(err);
         });
     }
-};
+}
+
+// getAllCourses('https://openeval-server.herokuapp.com/classes');
+
+/**
+ *
+ * Sends a post request to register a student for a course
+ *
+ * @param  courseNumber string, the department abbreviation and course number
+ * @param  courseName   string, the full descriptive name of the course
+ * @param  courseID     string, the mongo ID of the course
+ * @return none
+ *
+ */
+function registerCourse(courseNumber, courseName, courseID) {
+    const options = {
+        hostname: 'https://openeval-server.herokuapp.com/registeredCourses/cperez',
+        port: 443,
+        path: '/',
+        method: 'POST'
+    };
+    const req = https.request(options, (res) => {
+        console.log('statusCode: ', res.statusCode);
+        console.log('headers: ', res.headers);
+        res.on('data', (d) => {
+            process.stdout.write(d);
+        });
+    });
+    req.on('error', (e) => {
+        console.error(e);
+    });
+    req.end();
+}
